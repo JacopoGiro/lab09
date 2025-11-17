e83667ede914d60c50637af17a09e8b0b755be24
Jacopo Girolamini - jacopo.girolamini@studio.unibo.it <jacopo.girolamini@studio.unibo.it>
2025-11-17 12:46:13 +0100

    secondo esercizio incompleto fine LAB

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,34 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import javax.swing.JOptionPane;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    File currentFile = new File(System.getProperty("user.dir") + File.separator + "output.txt");
 
+    public void setFile(final File file){
+        currentFile = file;
+    }
+
+    public File getFile(){
+        return currentFile;
+    }
+
+    public String getPath(){
+        return currentFile.getAbsolutePath();
+    }
+
+    public void writeString(final String str) throws FileNotFoundException {
+        final PrintStream ps = new PrintStream(currentFile);
+        System.out.println("fatto " +  System.getProperty("user.dir") + File.separator + "output.txt");
+        ps.print(str);
+    }
 }
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,16 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,5 +19,43 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+    private final Controller controller = new Controller();
+    private static final int PROPORTION = 5;
+    
+    public SimpleGUI(){
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        JTextArea textArea = new JTextArea();
+        mainPanel.add(textArea, BorderLayout.CENTER);
+        JButton save = new JButton("Save");
+        mainPanel.add(save, BorderLayout.SOUTH);
 
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    controller.writeString(textArea.getText());
+                } catch (FileNotFoundException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+
+        frame.setContentPane(mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SimpleGUI n = new SimpleGUI();
+        n.display();
+    }
 }
